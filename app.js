522b6fc54ac38c0ce9ec6d1cff7cb0edd7790325
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-03-25 23:43:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const modulus = (event) => {
+    displayScreen.value += event.target.value
+}
+
+const division = (event) => {
+    displayScreen.value += event.target.value
+}
+
+const numberSeven = (event) => {
+    displayScreen.value += event.target.value
+}
+const numberEight = (event) => {
+    displayScreen.value += event.target.value
+}
+const numberNine = (event) => {
+    displayScreen.value += event.target.value
+}
+const multiplication = (event) => {
+    displayScreen.value += event.target.value
+}
+const numberFour = (event) => {
+    displayScreen.value += event.target.value
+}
+const numberFive = (event) => {
+    displayScreen.value += event.target.value
+}
+const numberSix = (event) => {
+    displayScreen.value += event.target.value
+}
+const numberOne = (event) => {
+    displayScreen.value += event.target.value
+}
+const numberTwo = (event) => {
+    displayScreen.value += event.target.value
+}
+const numberThree = (event) => {
+    displayScreen.value += event.target.value
+}
+const addition = (event) => {
+    displayScreen.value += event.target.value
+}
+const numberZero = (event) => {
+    displayScreen.value += event.target.value
+}
+const subtraction = (event) => {
+    displayScreen.value += event.target.value
+}
+const doubleZero = (event) => {
+    displayScreen.value += event.target.value
+}
+const fullStop = (event) => {
+    displayScreen.value += event.target.value
+}
+const clearScreen = () => {
+    displayScreen.value = ""
+}
+const deleteString = () => {
+    displayScreen.value = displayScreen.value.substr(0, displayScreen.value.length - 1)
+}
+const calculate = () => {
+    displayScreen.value = eval(displayScreen.value)
+}
